c11ae512d1933e723a7607f9cc436b4389d10bd5
NezhelskiyViktor <nezhelskiy@yandex.ru>
2024-08-18 10:04:39 +0300

    Добавил просмотр цен заказа по каждому заказу

--- flower_delivery/delivery/urls.py
+++ flower_delivery/delivery/urls.py
@@ -7,6 +7,6 @@ from . import views
 urlpatterns = [
     path('', views.deliver_view, name='deliver'),
     path('registration', views.registration_view, name='registration'),
-    path('login', views.login_view, name='login'),
+    path('entrance', views.entrance_view, name='entrance'),
     path('viewsrec', views.viewsrec_view, name='viewsrec'),
 ]  # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- flower_delivery/delivery/views.py
+++ flower_delivery/delivery/views.py
@@ -32,7 +32,7 @@ def registration_view(request):
     return render(request, 'delivery/registration.html', {'form': form})
 
 
-def login_view(request):
+def entrance_view(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -47,17 +47,29 @@ def login_view(request):
     else:
         form = LoginForm()
 
-    return render(request, 'login.html', {'form': form})
+    return render(request, 'entrance.html', {'form': form})
 
 
 def viewsrec_view(request):
-    users = User.objects.all()
-    products = Product.objects.all()
-    orders = Order.objects.all()
+    users = User.objects.all()  # покупатели
+    products = Product.objects.all()  # букеты
+    orders = Order.objects.all()  # заказы
+
+    allprice = 0.0  # общая стоимость
+    order_prices = {}  # словарь для хранения цен заказов
+
+    for order in orders:
+        order_total = 0.0  # общая стоимость для текущего заказа
+        for product in order.products.all():
+            order_total += float(product.price)
+        order_prices[order.id] = order_total  # сохранить цену для заказа
+        allprice += order_total  # добавляем к общей стоимости
 
     context = {
         'users': users,
         'products': products,
         'orders': orders,
+        'allprice': f"{allprice:.2f}" if allprice else allprice,
+        'order_prices': order_prices,  # передаем словарь с ценами заказов
     }
     return render(request, 'delivery/viewsrec.html', context)
